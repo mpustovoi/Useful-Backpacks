13d8750581b14737e2e3e2f7edd4706758f5c026
HyCraftHD <hycrafthd@live.de>
2024-08-02 00:35:35 +0200

    Move mixin to common and use for all loaders now

--- forge/src/main/java/info/u_team/useful_backpacks/init/UsefulBackpacksForgeCommonConstruct.java
+++ forge/src/main/java/info/u_team/useful_backpacks/init/UsefulBackpacksForgeCommonConstruct.java
@@ -2,10 +2,8 @@ package info.u_team.useful_backpacks.init;
 
 import info.u_team.u_team_core.api.construct.Construct;
 import info.u_team.u_team_core.api.construct.ModConstruct;
-import info.u_team.u_team_core.util.registry.BusRegister;
 import info.u_team.useful_backpacks.UsefulBackpacksReference;
 import info.u_team.useful_backpacks.config.ForgeCommonConfig;
-import info.u_team.useful_backpacks.event.ItemPickupEventHandler;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
 
@@ -15,7 +13,5 @@ public class UsefulBackpacksForgeCommonConstruct implements ModConstruct {
 	@Override
 	public void construct() {
 		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ForgeCommonConfig.CONFIG);
-		
-		BusRegister.registerForge(ItemPickupEventHandler::registerForge);
 	}
 }
