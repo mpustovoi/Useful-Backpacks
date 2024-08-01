272d80b225c9427914848c6578734e98594e57cb
HyCraftHD <hycrafthd@live.de>
2024-08-01 19:53:15 +0200

    Fix pickup event. Not good, but works with mixin for now

--- neoforge/src/main/java/info/u_team/useful_backpacks/init/UsefulBackpacksNeoForgeCommonConstruct.java
+++ neoforge/src/main/java/info/u_team/useful_backpacks/init/UsefulBackpacksNeoForgeCommonConstruct.java
@@ -2,10 +2,8 @@ package info.u_team.useful_backpacks.init;
 
 import info.u_team.u_team_core.api.construct.Construct;
 import info.u_team.u_team_core.api.construct.ModConstruct;
-import info.u_team.u_team_core.util.registry.BusRegister;
 import info.u_team.useful_backpacks.UsefulBackpacksReference;
 import info.u_team.useful_backpacks.config.NeoForgeCommonConfig;
-import info.u_team.useful_backpacks.event.ItemPickupEventHandler;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.config.ModConfig;
 
@@ -15,7 +13,5 @@ public class UsefulBackpacksNeoForgeCommonConstruct implements ModConstruct {
 	@Override
 	public void construct() {
 		ModLoadingContext.get().getActiveContainer().registerConfig(ModConfig.Type.COMMON, NeoForgeCommonConfig.CONFIG);
-		
-		BusRegister.registerNeoForge(ItemPickupEventHandler::registerNeoForge);
 	}
 }
