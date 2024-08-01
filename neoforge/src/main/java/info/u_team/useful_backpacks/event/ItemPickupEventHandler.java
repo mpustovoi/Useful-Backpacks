88a2609b97fa52e3786b39e8603a304e4f5638c2
HyCraftHD <hycrafthd@live.de>
2024-08-01 18:50:52 +0200

    Hopefully fix pickup on neoforge

--- neoforge/src/main/java/info/u_team/useful_backpacks/event/ItemPickupEventHandler.java
+++ neoforge/src/main/java/info/u_team/useful_backpacks/event/ItemPickupEventHandler.java
@@ -1,27 +1,33 @@
 package info.u_team.useful_backpacks.event;
 
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.common.util.TriState;
 import net.neoforged.neoforge.event.entity.player.ItemEntityPickupEvent;
 
 public class ItemPickupEventHandler {
 	
 	private static void onEntityItemPickup(ItemEntityPickupEvent.Pre event) {
-		final Player player = event.getPlayer();
+		final ItemEntity entity = event.getItemEntity();
+		if (entity.hasPickUpDelay()) {
+			return;
+		}
 		
+		final Player player = event.getPlayer();
 		if (!(player instanceof final ServerPlayer serverPlayer)) {
 			return;
 		}
 		
-		final ItemStack stackToPickup = event.getItemEntity().getItem();
+		final ItemStack stackToPickup = entity.getItem();
 		final ItemStack resultStack = ItemPickupCommonEventHandler.insertInBackpacks(serverPlayer, stackToPickup);
 		stackToPickup.setCount(resultStack.getCount());
 		
 		if (resultStack.isEmpty()) {
-			// event.setResult(); // TODO set something here??
+			event.setCanPickup(TriState.TRUE);
 		}
 	}
 	
